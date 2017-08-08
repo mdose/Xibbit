dbf14b635ce4a3a96753959793f26b59a5c5e87b
vagrant <vagrant@vagrant.vm>
2017-08-08 22:25:10 +0000

    Refactored Date and Measurement columns on artworks & cleaned on returns in __repr__ for artworks and artists

--- model.py
+++ model.py
@@ -12,44 +12,58 @@ db = SQLAlchemy()
 ##############################################################################
 # Compose ORM
 
-class Artwork(db.Model):
+class Art(db.Model):
     """Art model"""
 
     ___tablename__ = "artworks"
 
-    artwork_id = db.Column(db.Integer, primary_key=True, autoincrement=True,
-                           nullable=False)
+    art_id = db.Column(db.Integer, primary_key=True, autoincrement=True,
+                       nullable=False)
     title = db.Column(db.String(100), nullable=False)
-    image_url = db.Column(db.String(500), nullable=False)
-    # Find out if there is Datetime Library for BCE and CE dates and circa
-    # Need to store single years, year ranges, need ability to circa
-    # year = integer, year_range = integer, 'circa' = boolean,
-    # year_description = string(null)
-    date = db.Column(db.String(30), nullable=False)
+    # make images not nullable once you add urls to seed data!!!
+    image_url = db.Column(db.String(500), nullable=True)
+    # !!!Remember to add a constraint to db or server that insures that year or
+    # year description is required to add art to the database (all are nullable)
+    year = db.Column(db.Integer, nullable=True)
+    year_range = db.Column(db.Integer, nullable=True)
+    circa = db.Column(db.Boolean, nullable=True)
+    year_description = db.Column(db.String(50), nullable=True)
     medium = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    # Find out if there is a measurement Library for numbers (thinking ahead to 3.0)
-    # Either dimensions as strings or three integer fields plus 4th unit of meas field
-    height = db.Column(db.String(50), nullable=True)
-    width = db.Column(db.String(50), nullable=True)
-    # depth = db.Column(db.String(50), nullable=True)
-    collection_id = db.Column(db.Integer, nullable=False,
-                              db.ForeignKey('collections.collection_id'))
-    art_type_id = db.Column(db.Integer, nullable=False,
-                              db.ForeignKey('art_types.art_type_id'))
-    art_movement_id =
-    art_subject_matter_id =
+    # make descriptions not nullable once you add them to the seed data!!!
+    description = db.Column(db.String(500), nullable=True)
+    height_cm = db.Column(db.Float, nullable=True)
+    width_cm = db.Column(db.Float, nullable=True)
+    collection_id = db.Column(db.Integer,
+                              db.ForeignKey('collections.collection_id'),
+                              nullable=False)
+    art_type_id = db.Column(db.Integer,
+                            db.ForeignKey('art_types.art_type_id'),
+                            nullable=False)
+    art_movement_id = db.Column(db.Integer,
+                                db.ForeignKey('art_movments.art_movement_id'),
+                                nullable=False,)
+    subject_matter_id = db.Column(db.Integer,
+                                  db.ForeignKey('subject_matters.subject_matter_id'),
+                                  nullable=False)
 
 
     # establishes relationship thanks to the Foreign Key
     collection = db.relationship('Collection', backref='artworks')
-    art_type = db.relationship('ArtType', backref="art_types")
+    art_type = db.relationship('ArtType', backref='artworks')
+    art_movement = db.relationship('ArtMovement', backref='artworks')
+    subject_matter = db.relationship('SubjectMatter', backref='artworks')
 
     def __repr__(self):
         """Info on artworks"""
 
-        return "<Artwork id: {}, Title: {}, Image: {}, Date: {}, Medium: {}, Description: {}, Height {}, Witdh {}, Collection id: {}, ArtType id: {}>".format(
-            self.artwork_id, self.title, self.image_url, self.date, self.medium, self.description, self.height, self.width, self.collection_id, self.art_type_id)
+        return """
+        <Artwork id: {}, Title: {}, Image: {}, Date: {}, Medium: {}, Description: {},
+        Height {}, Witdh {}, Collection id: {}, ArtType id: {}, ArtMovement: {},
+        SubjectMatter: {}>""".format(self.artwork_id, self.title, self.image_url,
+                                     self.date, self.medium, self.description,
+                                     self.height, self.width, self.collection_id,
+                                     self.art_type_id, self.art_movement_id,
+                                     self.subject_matter_id)
 
 
 class Artist(db.Model):
@@ -58,26 +72,39 @@ class Artist(db.Model):
     ___tablename__ = "artists"
 
     artist_id = db.Column(db.Integer, primary_key=True, autoincrement=True,
-                           nullable=False)
-    # primary_name is the SURNAME of the artist, "Unknown artist", or ONLY
-    # artist name (ex. Myron of Ancient Greece)
+                          nullable=False)
+    # primary_name is the SURNAME of the artist, "Unknown artist", or ONLY artist name
     primary_name = db.Column(db.String(100), nullable=False)
     # secondary_name is the GIVEN name of the artist
     secondary_name = db.Column(db.String(100), nullable=True)
     birth_year = db.Column(db.Integer, nullable=True)
     death_year = db.Column(db.Integer, nullable=True)
-    bio = db.Column(db.String(500), nullable=False)
+    # make not nullable once bio is added!!!
+    bio = db.Column(db.String(500), nullable=True)
     image_url = db.Column(db.String(500), nullable=True)
 
     def __repr__(self):
         """Info on artists"""
 
         return "<Artist id: {}, Name: {}, {}, Lifespan: {} - {}, Bio: {}, Image: {}>".format(
-            self.artist_id, self.primary_name, self.secondary_name, self.birth_year, self.death_year, self.bio, self.image_url)
+            self.artist_id, self.primary_name, self.secondary_name, self.birth_year,
+            self.death_year, self.bio, self.image_url)
 
 
 class User(db.Model):
-    """User info"""
+    """User model"""
+
+    ___tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True,
+                        nullable=False)
+
+    email = db.Column(db.String(100), nullable=False)
+    secondary_name = db.Column(db.String(100), nullable=True)
+    birth_year = db.Column(db.Integer, nullable=True)
+    death_year = db.Column(db.Integer, nullable=True)
+    bio = db.Column(db.String(500), nullable=True)
+    image_url = db.Column(db.String(500), nullable=True)
 
     pass
 
@@ -113,7 +140,7 @@ class ArtistArtwork(db.Model):
     pass
 
 
-class UserArtwork(db.Model):
+class UserArt(db.Model):
     """Association table for Users and Artworks"""
 
     # list_id (2.0)
