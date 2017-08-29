f86f335f47346f390d7335b89fe0ac254f9d76a0
Megan <megan.e.dose@gmail.com>
2017-08-29 09:24:30 -0700

    WIP on Google Maps API/Refactored JS to be easier to read and and Info Windows

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,23 +1,62 @@
 "use strict";
 
-var map;
-var markers = [];
 // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 54.5260, lng: 15.2551},
-  // Europe Coordinates ^
-  // center: {lat: 27.467830, lng: -51.000000},
-  // World Coordinates; zoom: 2
-  // TODO: calc centerpt of all markers
-  zoom: 4
-}); 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 54.5260, lng: 15.2551},
+        // Europe Coordinates ^
+        // center: {lat: 27.467830, lng: -51.000000},
+        // World Coordinates; zoom: 2
+        // TODO: calc centerpt of all markers
+        zoom: 4
+    });
 
+    getMarkers(map, function(markers) {
+        console.log(markers); // debug info
+        var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    });
+}
 
-doAll();
-console.log(markers);
+function getMarkers(map, cb_done) {
+    $.get('/get_info', function(results) {
+        var markers = [];
+        console.log(results);
+        var marker;
+        var favorites = results['favorites'];
+        for (var one_favorite of favorites) {
+            marker = addMarker(map, one_favorite);
+            markers.push(marker);
+        }
+        cb_done(markers);
+    });
+}
 
+function addMarker(map, one_favorite) {
+    var LatLng = new google.maps.LatLng(one_favorite['lat'], one_favorite['lng'])
+    var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map,
+        title: one_favorite['title']
+    });
+    attachInfoWindow(map, marker, one_favorite);
+    return marker;
+}
 
+function attachInfoWindow(map, marker, one_favorite) {
+    var contentString = '<div id="content">' +
+        '<h1>Test</h1>' +
+        '</div>';
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 200
+    });
+
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });  
+}
 
 // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // conssole.log("I'm here");
@@ -36,54 +75,5 @@ console.log(markers);
         // Add a marker clusterer to manage the markers.
 // var markerCluster = new MarkerClusterer(map, markers,
 //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-}
 
-
-function getInfo(_callback){
-$.get('/get_info', function(results){
-    console.log(results);
-    var marker;
-    var favorites = results['favorites'];
-    for (var one_favorite of favorites){
-        marker = addMarker(one_favorite);
-        markers.push(marker);
-    }
-    _callback();
-});
-}
-
-function addMarker(one_favorite) {
-  var LatLng = new google.maps.LatLng(one_favorite['lat'], one_favorite['lng'])
-  var marker = new google.maps.Marker({
-      position: LatLng,
-      map: map,
-      title: one_favorite['title']
-   });
-  return marker;
-}
-
-function doAll(){
- getInfo(function() { 
-    var markerCluster = new MarkerClusterer(map, markers,
-    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-});
-}
-
-// function addInfoWindow() {
-
-//   var contentString = '<div id="content">' +
-//     '<h1>Test</h1>' +
-//     '</div>';
-
-//   var infoWindow = new google.maps.InfoWindow({
-//     content: contentString,
-//     maxWidth: 200
-//   });
-
-//   var marker = ?
-
-//   marker.addListener('click', function() {
-//   infoWindow.open(map, marker);
-//   });  
-// }
 
