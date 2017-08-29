3481061bf973ce5f3a40988e5380e5227b486dce
Megan <megan.e.dose@gmail.com>
2017-08-28 17:04:03 -0700

    WIP on Google Maps API/Changed map markers to marker clusterers

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,29 +1,55 @@
 "use strict";
 
 var map;
+var markers = [];
+// var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 function initMap() {
 map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 27.467830, lng: -51.000000},
+  center: {lat: 54.5260, lng: 15.2551},
+  // Europe Coordinates
+  // center: {lat: 27.467830, lng: -51.000000},
+  // World Coordinates; zoom: 2
   // TODO: calc centerpt of all markers
-  zoom: 2
+  zoom: 4
 }); 
 
-getInfo();
+
+doAll();
+console.log(markers);
+
+
+
+// var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // conssole.log("I'm here");
 // var louvreMarker = new google.maps.Marker({
 //   position: {lat: 48.860621, lng: 2.337642},
 //   map: map
 // });
+// var markers = getInfo();
+//           return new google.maps.Marker({
+//             position: LatLng,
+//             label: labels[i % labels.length]
+//           });
+//         });
+        
+
+        // Add a marker clusterer to manage the markers.
+// var markerCluster = new MarkerClusterer(map, markers,
+//     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 
-function getInfo(){
+
+function getInfo(_callback){
 $.get('/get_info', function(results){
     console.log(results);
     var marker;
     var favorites = results['favorites'];
     for (var one_favorite of favorites){
         marker = addMarker(one_favorite);
-    }});
+        markers.push(marker);
+    }
+    _callback();
+});
 }
 
 function addMarker(one_favorite) {
@@ -32,6 +58,13 @@ function addMarker(one_favorite) {
       position: LatLng,
       map: map,
       title: one_favorite['title'],
-  });
+   });
   return marker;
+}
+
+function doAll(){
+ getInfo(function() { 
+    var markerCluster = new MarkerClusterer(map, markers,
+    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+});
 }
